1c1b9487b11c6336efe92aadb2c0087bdc9626d8
Evanosity <iamevanosity@gmail.com>
2017-12-01 13:24:22 -0500

    more framework work
    basically nothing tbh

--- src/remoteAccessDatabaseClient/Networking.java
+++ src/remoteAccessDatabaseClient/Networking.java
@@ -1,19 +1,26 @@
 package remoteAccessDatabaseClient;
 
+/**
+ * public class Networking - this is the framework for networking, and it just has the very basic receive/send methods. These will be used
+ * by other classes to effectively send and receive data.
+ * @author Evanosity
+ * @date December 1 2017
+ */
 public class Networking {
-	public void begin(){
+	public Networking(){
+		//setup
+	}
+	
+	public String receiveMessage(){
+		//this will receive a message, or wait to receive a message.
+		return null;
+	}
+	
+	public void sendMessage(String toSend){
 		
 	}
 	
-	public void close(){
-		
-	}
-	
-	public void receive(){
-		
-	}
-	
-	public void send(){
+	public void shutdown(){
 		
 	}
 }
--- src/remoteAccessDatabaseClient/Parser.java
+++ src/remoteAccessDatabaseClient/Parser.java
@@ -5,17 +5,30 @@ package remoteAccessDatabaseClient;
  * @author Evanosity
  * @date November 29 2017
  */
+/**
+ * okay so
+ * this is how it's gonna go.
+ * One message in the front, saying the array size. Networking class will then prepare to receive that many messages, and this class will create an array of this size.
+ * As the packets come in, the array will recompile it based off of the info at the start of each packet.
+ * 
+ * As well, there is a chance we will need to receive a list of all of the column headers. However, I think it would be easier to store that locally.
+ *
+ * String message="1|2|3|<data here>"
+ */
 public class Parser {
-	public Parser(){
-		/**
-		 * okay so
-		 * this is how it's gonna go.
-		 * One message in the front, saying the array size. Networking class will then prepare to receive that many messages, and this class will create an array of this size.
-		 * As the packets come in, the array will recompile it based off of the info at the start of each packet.
-		 * 
-		 * As well, there is a chance we will need to receive a list of all of the column headers. However, I think it would be easier to store that locally.
-		 *
-		 * String message="1|2|3|<data here>
-		 */
+	//there is actually a good chance this class might need to be mirrored on the server side; however, as of right now I am unsure how the database access is being done.
+	private String[][]raw;
+	private String[][]toReturn;
+	public Parser(String[][] given){
+		raw=given;
+	}
+	
+	public String[][]getArray(){
+		formatArray();
+		return toReturn;
+	}
+	
+	private void formatArray(){
+		toReturn=raw;
 	}
 }
