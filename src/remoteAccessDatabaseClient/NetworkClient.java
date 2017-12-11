010f318f8c46c7a3b805e18d5d890e8014f39042
Evanosity <iamevanosity@gmail.com>
2017-12-11 13:17:38 -0500

    Build the network framework
    I guess you could say this was a lot of work ;)

---
+++ src/remoteAccessDatabaseClient/NetworkClient.java
@@ -0,0 +1,69 @@
+package remoteAccessDatabaseClient;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import java.net.Socket;
+
+/**
+ * public class NetworkClient - this is the framework for networking, and it just has the very basic receive/send methods. These will be used
+ * by other classes to effectively send and receive data.
+ * @author Evanosity
+ * @date December 11 2017
+ */
+public class NetworkClient {
+	private Socket client;
+	
+	private DataInputStream receive;
+	private DataOutputStream send;
+	
+	private int port;
+	private String host;
+	
+	/**
+	 * public NetworkClient - this is the constructor that sets up the class.
+	 * @param portNumber - the port to connect on
+	 * @param hostName - the host to connect to
+	 */
+	public NetworkClient(int portNumber, String hostName){
+		port=portNumber;
+		host=hostName;
+		
+		try{
+			client=new Socket(host, port);
+			
+			send=new DataOutputStream(client.getOutputStream());
+			receive=new DataInputStream(client.getInputStream());
+		}
+		catch(IOException e){
+			System.out.println("Server not on?");
+		}
+	}
+	
+	/**
+	 * public String receiveMessage - this method will read an input from the server, then return it.
+	 * @return the received message
+	 * @throws IOException
+	 */
+	public String receiveMessage()throws IOException{
+		return receive.readUTF();
+	}
+	
+	/**
+	 * public void sendMessage - this method will send a message to the server.
+	 * @param toSend - the message to send.
+	 * @throws IOException
+	 */
+	public void sendMessage(String toSend)throws IOException{
+		send.writeUTF(toSend);
+	}
+	
+	/**
+	 * public void shutdown - closes the connection in a graceful fashion.
+	 * @throws IOException
+	 */
+	public void shutdown()throws IOException{
+		client.close();
+	}
+}
