73679f51bbe6a784cf88da0b57ec82a2330c5072
Evanosity <iamevanosity@gmail.com>
2017-12-14 13:52:40 -0500

    pls

--- src/remoteAccessDatabaseClient/Main.java
+++ src/remoteAccessDatabaseClient/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		//ClientSide csl = new ClientSide();
 		//csl.ClientSideLogin();
-		NetworkClient nc=new NetworkClient("192.168.0.1",6066);
+		NetworkClient nc=new NetworkClient("192.168.0.100",6066);
 		nc.sendMessage("testing 1 2 3 testing");
 	}
 }
