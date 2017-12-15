efe9ad171d54bc4d8219c5644abe74e4788b4e68
camham <31860003+camham@users.noreply.github.com>
2017-12-15 12:30:53 -0500

    Uncommented netowrking

--- src/remoteAccessDatabaseClient/Main.java
+++ src/remoteAccessDatabaseClient/Main.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		//ClientSide csl = new ClientSide();
-		//csl.ClientSideLogin();
+		ClientSide csl = new ClientSide();
+		csl.ClientSideLogin();
+		
 		NetworkClient nc=new NetworkClient("192.168.0.100",6066);
 		nc.sendMessage("testing 1 2 3 testing");
 	}
