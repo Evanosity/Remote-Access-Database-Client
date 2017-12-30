39dca6161789c1f4cc8672b13ba998bdd567982d
Bluedog2121 <33208732+Bluedog2121@users.noreply.github.com>
2017-12-30 15:28:47 -0500

    Added recieve statement

--- src/remoteAccessDatabaseClient/Main.java
+++ src/remoteAccessDatabaseClient/Main.java
@@ -3,12 +3,15 @@ package remoteAccessDatabaseClient;
 import java.io.IOException;
 
 public class Main {
-
+	
+	private static String tableContents;
+	
 	public static void main(String[] args) throws IOException {
 		ClientSide csl = new ClientSide();
 		csl.ClientSideLogin();
 		
 		NetworkClient nc=new NetworkClient("192.168.0.100",6066);
-		nc.sendMessage("testing 1 2 3 testing");
+		
+		nc.receiveMessage();
 	}
 }
