6340b88b75f3a008175410116cc6f99fb39faed1
Evanosity <iamevanosity@gmail.com>
2018-01-02 20:22:18 -0500

    mini todo list
    added a tiny todo list so we can keep track properly

--- src/remoteAccessDatabaseClient/Main.java
+++ src/remoteAccessDatabaseClient/Main.java
@@ -3,7 +3,12 @@ package remoteAccessDatabaseClient;
 import java.io.IOException;
 
 public class Main {
-	
+	/**
+	 * todo
+	 * -add the method for properly receiving arrays
+	 * -hook up the editor thingy for the database contents on the GUI. Should be simple
+	 * -Properly flag which stuff needs to be updated/added/deleted/whatever to the database
+	 */
 	private static String tableContents;
 	
 	public static void main(String[] args) throws IOException {
